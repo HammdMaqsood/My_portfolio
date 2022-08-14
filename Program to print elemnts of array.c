385ad7635338f0a350e986a004b170fb94d6a0da
Hammad <55254990+HammdMaqsood@users.noreply.github.com>
2022-08-15 02:30:12 +0500

    Add files via upload
    Program in c language to print all elements of an array.

---
+++ Program to print elemnts of array.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    int z[5]={1,3,5,7,9};
+    int i;
+ 
+    int j=0;
+
+    for(i=0;i<5;i++)
+    {
+        printf("\n The value no %d is = %d",j,z[i]);
+        j++;
+}
+    return 0;
+}
+
